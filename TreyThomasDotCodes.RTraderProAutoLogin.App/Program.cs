6d0e5c509d2b41b5b81eea785a00babe82ec18f9
Trey Thomas <trey@treythomas.codes>
2022-06-22 11:52:01 -0500

    Add project files.

---
+++ TreyThomasDotCodes.RTraderProAutoLogin.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
+
+namespace TreyThomasDotCodes.RTraderProAutoLogin.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var automation = new UIA3Automation())
+            using (var app = Application.AttachOrLaunch(new System.Diagnostics.ProcessStartInfo(@"C:\Program Files (x86)\Rithmic\Rithmic Trader Pro\Rithmic Trader Pro.exe")))
+            {
+                var window = app.GetMainWindow(automation);
+                var loginButton = window.FindFirstDescendant(cf => cf.ByText("Login")).AsButton();
+
+                loginButton.Invoke();
+            }
+        }
+    }
+}
